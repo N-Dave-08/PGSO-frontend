94ed7d81dd8dc6b4f32b89cb3eeb1d8324e1f180
N-Dave-08 <cdave.dev@gmail.com>
2024-11-27 19:38:45 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-//   images: {
-//     domains: ['dev.pgso.bpc-bsis4d.com'],
-//   },
-//   output: "export",
-//   basePath: process.env.BASE_PATH ? process.env.BASE_PATH : "",
-//   assetPrefix: process.env.URL ? process.env.URL : undefined,
-// };
+  images: {
+    domains: ['dev.pgso.bpc-bsis4d.com'],
+  },
+  output: "export",
+  basePath: process.env.BASE_PATH ? process.env.BASE_PATH : "",
+  assetPrefix: process.env.URL ? process.env.URL : undefined,
+};
 
 export default nextConfig;
