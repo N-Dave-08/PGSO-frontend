d7d7e907453c26121a079dde3c5c5191e4837684
N-Dave-08 <cdave.dev@gmail.com>
2024-11-27 17:12:12 +0800

    test

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -6,6 +6,7 @@ export default function Hero() {
         <>
             <AnimatedBackground />
             <section className='relative z-10 flex flex-col justify-center items-center h-screen gap-6 -mt-[6.5rem] px-40'>
+                <p>kajbsdkasbjdkajsbdkjabsdkjabsdkjb</p>
                 <div className='text-center space-y-2'>
                     <h1 className='text-5xl font-bold'>Provincial General Service Office Request Management System</h1>
                     <p className='text-xl'>Optimizing Government Services for Faster Request Processing</p>
