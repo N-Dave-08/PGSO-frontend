a901099c36aaac29d145684da29ff7cde2901de3
N-Dave-08 <cdave.dev@gmail.com>
2024-12-01 22:05:15 +0800

    output export becomes comment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  // output: "export",
   basePath: process.env.BASE_PATH ? process.env.BASE_PATH : "",
   assetPrefix: process.env.URL ? process.env.URL : undefined,
 };
