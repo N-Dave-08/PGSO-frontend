47c30162f92a51dbd9ba1c871297267a88ec11a6
N-Dave-08 <cdave.dev@gmail.com>
2024-12-02 02:49:55 +0800

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import CTA from "@/components/sections/CTA";
 export default function Home() {
   return (
     <main className="min-h-screen">
+      asdasd
       <Navbar />
       <Hero />
       <Capabilities />
