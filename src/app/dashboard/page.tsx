dd79394a302866c78413a648025446e3ce9d2535
N-Dave-08 <cdave.dev@gmail.com>
2024-12-03 01:01:17 +0800

    test

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -30,7 +30,7 @@ export default function Dashboard() {
 
   return (
     <div>
-      <h1 className="text-2xl font-bold mb-4">Welcome to the Dashboard, asjdbaskdbj{user.email}</h1>
+      <h1 className="text-2xl font-bold mb-4">Welcome to the Dashboard, {user.email}</h1>
       <p>Your role is: {role}</p>
     </div>
   )
