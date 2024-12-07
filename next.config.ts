455d12727bc78764b880d49e4e7082667831a748
N-Dave-08 <cdave.dev@gmail.com>
2024-12-08 04:20:24 +0800

    try fix refresh error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  output: "export",
   basePath: process.env.BASE_PATH ? process.env.BASE_PATH : "",
   assetPrefix: process.env.URL ? process.env.URL : undefined,
   trailingSlash: true,
