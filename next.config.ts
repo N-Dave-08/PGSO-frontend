fd748855fb440c132db151d079005c3362722dbe
N-Dave-08 <cdave.dev@gmail.com>
2024-12-09 03:35:08 +0800

    fix assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   output: "export",
   distDir: 'dist',
   basePath: '',
-  assetPrefix: './',
+  assetPrefix: 'https://dev.pgso.bpc-bsis4d.com',
   // basePath: process.env.BASE_PATH ? process.env.BASE_PATH : "",
   // assetPrefix: process.env.URL ? process.env.URL : undefined,
   trailingSlash: true,
