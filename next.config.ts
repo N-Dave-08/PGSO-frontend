5b6f250bc88f0b7c6b95ec3f143e459a20c5c2df
N-Dave-08 <cdave.dev@gmail.com>
2024-12-09 03:50:46 +0800

    change back everything

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  distDir: 'dist',
-  basePath: '',
-  assetPrefix: 'https://dev.pgso.bpc-bsis4d.com',
   // basePath: process.env.BASE_PATH ? process.env.BASE_PATH : "",
   // assetPrefix: process.env.URL ? process.env.URL : undefined,
   trailingSlash: true,
