11a05b201834a52473c3e37b2125eda37862c39e
N-Dave-08 <cdave.dev@gmail.com>
2024-12-09 03:39:03 +0800

    fix apostrophy

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
         <h1 className="text-4xl font-bold">404</h1>
         <h2 className="text-xl mt-4">Page Not Found</h2>
         <p className="mt-2 text-muted-foreground">
-          The page you're looking for doesn't exist or has been moved.
+          The page you&apos;re looking for doesn&apos;texist or has been moved.
         </p>
       </div>
     );
