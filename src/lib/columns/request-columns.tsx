6673295abfdd9f155c6cd4100b891914f683b948
N-Dave-08 <cdave.dev@gmail.com>
2024-12-11 03:26:44 +0800

    fix eslint

--- src/lib/columns/request-columns.tsx
+++ src/lib/columns/request-columns.tsx
@@ -1,5 +1,5 @@
 import { ColumnDef } from "@tanstack/react-table"
-import { ArrowUpDown, MoreHorizontal, Trash, PenSquare, User } from 'lucide-react'
+import { ArrowUpDown, MoreHorizontal, Trash, PenSquare } from 'lucide-react'
 import { Button } from "@/components/ui/button"
 import { Checkbox } from "@/components/ui/checkbox"
 import {
