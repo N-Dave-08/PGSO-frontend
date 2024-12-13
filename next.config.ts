538f38fe2bee93540eb36064d7f387ba909e22e4
N-Dave-08 <cdave.dev@gmail.com>
2024-12-14 06:25:12 +0800

    remove rewrites

--- next.config.ts
+++ next.config.ts
@@ -7,27 +7,27 @@ const nextConfig: NextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true,
-  },
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
-        ],
-      },
-    ];
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://server.pgso.bpc-bsis4d.com/public/api/:path*'
-      }
-    ]
-  }
+  // },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       headers: [
+  //         { key: 'Access-Control-Allow-Origin', value: '*' },
+  //         { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,OPTIONS' },
+  //         { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+  //       ],
+  //     },
+  //   ];
+  // },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: 'https://server.pgso.bpc-bsis4d.com/public/api/:path*'
+  //     }
+  //   ]
+  // }
 };
 
 export default nextConfig;
