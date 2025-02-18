957fa9e36d94f083baf879b15b863be8acda1d4c
N-Dave-08 <cdave.dev@gmail.com>
2025-02-19 07:33:01 +0800

    updated framework versions

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
-    darkMode: ["class"],
+    darkMode: ["class", "class"],
     content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -30,11 +30,7 @@ export default {
   		}
   	}
   },
-  daisyui: {
-    themes: ["night"],
-  },
   plugins: [
-    require("daisyui"),
       require("tailwindcss-animate")
 ],
 } satisfies Config;
