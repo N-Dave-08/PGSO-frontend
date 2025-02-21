23789c090ad9fc1f75a693a1f1e3be71c9e75dc9
N-Dave-08 <cdave.dev@gmail.com>
2025-02-22 05:30:54 +0800

    remove bg

--- src/components/AnimatedBackground.tsx
+++ src/components/AnimatedBackground.tsx
@@ -1,67 +1,66 @@
-'use client'
+// 'use client'
 
-import React, { useEffect } from 'react'
+// import React, { useEffect } from 'react'
 
-export default function AnimatedBackground(): JSX.Element {
-  useEffect(() => {
-    let curX = 0;
-    let curY = 0;
+// export default function AnimatedBackground(): TSX.Element {
+//   useEffect(() => {
+//     let curX = 0;
+//     let curY = 0;
 
-    let tgX = 0;
-    let tgY = 0;
+//     let tgX = 0;
+//     let tgY = 0;
 
-    const interBubble = document.querySelector('.interactive') as HTMLElement | null;
+//     const interBubble = document.querySelector('.interactive') as HTMLElement | null;
 
-    function move() {
-      curX += (tgX - curX) / 20;
-      curY += (tgY - curY) / 20;
+//     function move() {
+//       curX += (tgX - curX) / 20;
+//       curY += (tgY - curY) / 20;
 
-      if (interBubble) {
-        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
-      }
+//       if (interBubble) {
+//         interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+//       }
 
-      requestAnimationFrame(move);
-    }
+//       requestAnimationFrame(move);
+//     }
 
-    const handleMouseMove = (event: MouseEvent) => {
-      tgX = event.clientX;
-      tgY = event.clientY;
-    };
+//     const handleMouseMove = (event: MouseEvent) => {
+//       tgX = event.clientX;
+//       tgY = event.clientY;
+//     };
 
-    window.addEventListener('mousemove', handleMouseMove);
+//     window.addEventListener('mousemove', handleMouseMove);
 
-    move();
+//     move();
 
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, []);
+//     return () => {
+//       window.removeEventListener('mousemove', handleMouseMove);
+//     };
+//   }, []);
 
-  return (
-    <div className="gradient-bg">
-      <svg id='svg1' xmlns="http://www.w3.org/2000/svg">
-        <defs>
-          <filter id="goo">
-            <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
-            <feColorMatrix 
-              in="blur" 
-              type="matrix" 
-              values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 18 -8"
-              result="goo" 
-            />
-            <feBlend in="SourceGraphic" in2="goo" />
-          </filter>
-        </defs>
-      </svg>
-      <div className="gradient-container">
-        <div className="circle-1"></div>
-        <div className="circle-2"></div>
-        <div className="circle-3"></div>
-        <div className="circle-4"></div>
-        <div className="circle-5"></div>
-        <div className="interactive"></div>
-      </div>
-    </div>
-  )
-}
-
+//   return (
+//     <div className="gradient-bg">
+//       <svg id='svg1' xmlns="http://www.w3.org/2000/svg">
+//         <defs>
+//           <filter id="goo">
+//             <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+//             <feColorMatrix
+//               in="blur"
+//               type="matrix"
+//               values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 18 -8"
+//               result="goo"
+//             />
+//             <feBlend in="SourceGraphic" in2="goo" />
+//           </filter>
+//         </defs>
+//       </svg>
+//       <div className="gradient-container">
+//         <div className="circle-1"></div>
+//         <div className="circle-2"></div>
+//         <div className="circle-3"></div>
+//         <div className="circle-4"></div>
+//         <div className="circle-5"></div>
+//         <div className="interactive"></div>
+//       </div>
+//     </div>
+//   )
+// }
