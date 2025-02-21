e2f51cddd64b42cf804208be097b588393118777
N-Dave-08 <cdave.dev@gmail.com>
2025-02-22 04:56:51 +0800

    fix eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,7 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Hero from "@/components/sections/Hero";
 import Navbar from "@/components/navbars/Navbar";
-import Capabilities from "@/components/sections/Capabilities";
-import Process from "@/components/sections/Process";
-import FAQ from "@/components/sections/FAQ";
 import Footer from "@/components/sections/Footer";
-import CTA from "@/components/sections/CTA";
 
 export default function Home() {
   const router = useRouter();
