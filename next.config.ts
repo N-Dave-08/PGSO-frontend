d6b7160e7ec370575894423eba8cce837dc01e0d
N-Dave-08 <cdave.dev@gmail.com>
2025-02-22 13:10:16 +0800

    bring back next.config

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  // basePath: process.env.BASE_PATH ? process.env.BASE_PATH : "",
+  // assetPrefix: process.env.URL ? process.env.URL : undefined,
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  basePath: "",
-  distDir: "dist",
 };
 
 export default nextConfig;
